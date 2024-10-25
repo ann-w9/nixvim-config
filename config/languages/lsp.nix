08089db557e41aefd448ede89778e30228949d2f
aes <>
2024-10-25 19:50:37 -0300

    feat: add lsp servers and keybinds

--- config/languages/lsp.nix
+++ config/languages/lsp.nix
@@ -3,10 +3,138 @@
     lsp = {
       enable = true;
       servers = {
+        # generic
+        # c
+        clangd.enable = true;
+
+        # style / css
+        cssls.enable = true;
+        # cssmodules_ls.enable = true;
+        # css_variables.enable = true;
+        # stylelint.enable = true;
+        # somesass_ls.enable = true;
+
+        # nix
         nixd.enable = true;
         nil-ls.enable = true;
+        statix.enable = true;
+
+        # python
         pyright.enable = true;
         ruff-lsp.enable = true;
+
+        # typescript / js
+        biome.enable = true;
+        ts_ls = {
+          enable = true;
+          autostart = true;
+          filetypes = [
+            "javascript"
+            "typescript"
+            "javascriptreact"
+            "typescriptreact"
+          ];
+          extraOptions = {
+            settings = {
+              javascript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTypeHints = true;
+                  includeInlayVariableTypeHintsWhenTypeMatchesName = true;
+                };
+              };
+              typescript = {
+                inlayHints = {
+                  includeInlayEnumMemberValueHints = true;
+                  includeInlayFunctionLikeReturnTypeHints = true;
+                  includeInlayFunctionParameterTypeHints = true;
+                  includeInlayParameterNameHints = "all";
+                  includeInlayParameterNameHintsWhenArgumentMatchesName = true;
+                  includeInlayPropertyDeclarationTypeHints = true;
+                  includeInlayVariableTgpeHints = true;
+                  includeInlayVariableTypeHintsWhenTypeMatchesName = true;
+                };
+              };
+            };
+          };
+        };
+
+        # html
+        html.enable = true;
+        # emmet.enable = true;
+
+        #shell
+        bashls.enable = true;
+        # fish_lsp.enable = true;
+
+        # others
+        marksman.enable = true;
+        lua_ls.enable = true;
+        zk.enable = true;
+        yamlls.enable = true;
+        texlab.enable = true;
+        jsonls.enable = true;
+        # gdscript.enable = true;
+      };
+      keymaps = {
+        silent = true;
+        lspBuf = {
+          gd = {
+            action = "definition";
+            desc = "Goto Definition";
+          };
+          gr = {
+            action = "references";
+            desc = "Goto References";
+          };
+          gD = {
+            action = "declaration";
+            desc = "Goto Declaration";
+          };
+          gI = {
+            action = "implementation";
+            desc = "Goto Implementation";
+          };
+          gT = {
+            action = "type_definition";
+            desc = "Type Definition";
+          };
+          K = {
+            action = "hover";
+            desc = "Hover";
+          };
+          "<leader>cw" = {
+            action = "workspace_symbol";
+            desc = "Workspace Symbol";
+          };
+          "<leader>cr" = {
+            action = "rename";
+            desc = "Rename";
+          };
+          "<leader>ca" = {
+            action = "code_action";
+            desc = "Code Action";
+          };
+        };
+        diagnostic = {
+          "<leader>cd" = {
+            action = "open_float";
+            desc = "Line Diagnostics";
+          };
+          "[d" = {
+            action = "goto_next";
+            desc = "Next Diagnostic";
+          };
+          "]d" = {
+            action = "goto_prev";
+            desc = "Previous Diagnostic";
+          };
+        };
       };
     };
   };
--- config/languages/none-ls.nix
+++ config/languages/none-ls.nix
@@ -8,17 +8,26 @@
       sources = {
         code_actions = {
           refactoring.enable = true;
+          gitsigns.enable = true;
           statix.enable = true;
         };
 
         diagnostics = {
+          checkstyle.enable = true;
           deadnix.enable = true;
           statix.enable = true;
+          yamllint.enable = true;
+          fish.enable = true;
+          stylelint.enable = true;
           # ruff.enable = true;
         };
 
         formatting = {
           alejandra.enable = true;
+          # nixfmt.enable = true;
+          biome.enable = true;
+          # clang = true;
+          stylelint.enable = true;
           # ruff.enable = true;
           isort.enable = true;
           # pyflyby.enable = true;
